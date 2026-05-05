d9554e51dc05617198db5ae06cfeb515ddb841a0
Sean O'Brien <sacredobelisk@users.noreply.github.com>
2026-05-04 21:48:18 -0400

    Add Netlify redirects (#12)

--- app/components/about/index.tsx
+++ app/components/about/index.tsx
@@ -8,8 +8,8 @@ export const About = () => (
     <img src={image} alt="Digital illustration of Ally as a kid" />
 
     <p>
-      Hi! I&apos;m Ally OBrien, a retouch artist and graphic designer based outside of Philadelphia. I specialize in
-      high-end retouching, color correction, logo design, print design, and web graphics.
+      Hi! I&apos;m Ally O&apos;Brien, a retouch artist and graphic designer based outside of Philadelphia. I specialize
+      in high-end retouching, color correction, logo design, print design, and web graphics.
     </p>
   </div>
 );
